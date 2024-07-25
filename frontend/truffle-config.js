7dcc31a0cada777d0bdefd77b20093da723fb73d
Martin Emil Brabenec <martinemilbrabenec@gmail.com>
2024-07-25 23:37:19 +0200

    updates to be able to use openzeppelin

--- frontend/truffle-config.js
+++ frontend/truffle-config.js
@@ -1,14 +1,23 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "5777",
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "5777",       // Any network (default: none)
     },
   },
+
+  // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.0",
-    },
-  },
+      version: "pragma",    // Use the pragma statement from the contracts
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        },
+        evmVersion: "istanbul"
+      }
+    }
+  }
 };
